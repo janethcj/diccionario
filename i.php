8bf585e18d146f0e2e7a5bc63fd78cd9c3479572
Janeth CJ <78242681+janethcj@users.noreply.github.com>
2022-05-19 10:50:43 -0500

    Create i.php

---
+++ i.php
@@ -0,0 +1,20 @@
+<?php
+    
+	echo "Importancia";
+	echo "<br>";
+
+    echo "Valor, interés o influencia de una cosa";
+    echo "<br>";
+
+  echo "Inteligencia";
+	echo "<br>";
+
+    echo "Facultad de la mente que permite aprender, entender, razonar, tomar decisiones y formarse una idea determinada de la realidad";
+	  echo "<br>";
+
+	echo "Inherente";
+	echo "<br>";
+
+    echo "Que es esencial y permanente en un ser o en una cosa o no se puede separar de él";
+	  echo "<br>";
+?>
