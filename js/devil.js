07eb416ddb3b551ae86ce89984114a6b880ccec4
janethcj <78242681+janethcj@users.noreply.github.com>
2022-05-22 17:56:46 -0500

    Update devil.js
    Add function letter i

--- js/devil.js
+++ js/devil.js
@@ -35,6 +35,25 @@ $(document).ready(function() {
 });
 
 $(document).ready(function() {
+	$('#letter-i a').click(function(event) {
+		event.preventDefault();
+		$.getJSON('i.json', function(data) {
+			var html = '';
+			$.each(data, function(entryIndex, entry) {
+				html += '<div class="entry">';
+				html += '<h3 class="term">' + entry.term + '</h3>';
+				html += '<div class="part">' + entry.part + '</div>';
+				html += '<div class="definition">';
+				html += entry.definition;
+				html += '</div>';
+				html += '</div>';
+			});
+			$('#dictionary').html(html);
+		});
+	});
+});
+
+$(document).ready(function() {
 	$('#letter-j j').click(function(event) {
 		event.preventDefault();
 		$('#dictionary').load('j.html');
